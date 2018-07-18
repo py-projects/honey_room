227d3463878031ae9df484fd876d1206fa98876f
fairyland-cat <975280890@qq.com>
2018-07-18 20:16:24 +0800

    '分支提交'

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from main.models import *
 
 # Create your views here.
+
+
+def cake(request):
+    argument = request.GET
+    feild = argument.get('type')
+    value = argument.get('value')
+    if not argument:
+        cakes = Cake.objects.all()
+        return render(request, 'products.html', {'cakes': cakes,
+                                                 'classify': '全部商品'})
+    return None
