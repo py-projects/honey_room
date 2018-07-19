523e14530fa6f6d4d334f02a6501a178e6874c05
fairyland-cat <975280890@qq.com>
2018-07-19 11:47:51 +0800

    'base.html、商品列表'

--- apps/main/views.py
+++ apps/main/views.py
@@ -4,12 +4,18 @@ from main.models import *
 # Create your views here.
 
 
+# 返回商品数据
 def cake(request):
+    cakes = None
     argument = request.GET
-    feild = argument.get('type')
+    field = argument.get('type')
     value = argument.get('value')
     if not argument:
         cakes = Cake.objects.all()
-        return render(request, 'products.html', {'cakes': cakes,
-                                                 'classify': '全部商品'})
-    return None
+    elif field == 'cake_relation':
+        cakes = Cake.objects.filter(cake_relation__relation=value)
+    images = [x.cake_img.split(',') for x in cakes]
+    cakes = zip(cakes, images)
+    print(cakes, '------------------------------')
+    return render(request, 'products.html', {'cakes': cakes,
+                                             'classify': '全部商品'})
